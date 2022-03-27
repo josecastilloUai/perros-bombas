5cf5de728349a907e701073e1ea4364e6d276787
josecastilloUai <95188754+josecastilloUai@users.noreply.github.com>
2022-03-27 09:26:36 -0300

    encode
    manito llévame pa la casa

--- Tarea1 Estructura.c
+++ Tarea1 Estructura.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
+void codificar(char texto[], char str1[], char str2[]){
+  for(int i=0;i<9;i++){
+    char ct=texto[i];
+    for (int e=0;e<3;e++){
+      char p1=str1[e];
+      char p2=str2[e];
+      if(p1==ct){
+        texto[i]=p2;
+      }
+      else if(p2==ct){
+        texto[i]=p1;
+      }
+    }
+  }
+  printf("Texto : \n");
+  printf("%s",texto);
+}
+
 void enigma(char nombreE[], char accion[], char palabra1[], char palabra2[], char nombreS[]) {
   
   printf("\n%s\n", nombreE);
